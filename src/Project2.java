ee4cfc3ad829ebd18eea0a701c8401ba31d59aa0
Adamantcheese <random123x@gmail.com>
2015-03-11 20:06:18 -0700

    added more test files, they all work as expected.

--- src/Project2.java
+++ src/Project2.java
@@ -11,6 +11,7 @@ public class Project2 {
         //Parse each file one at a time, showing output as needed.
 
         //ToyTestFile is the file given from Project1. It will always reject due to syntax errors.
+        //It is a test of incorrect class declaration structure as well.
         System.out.println("FIRST FILE PARSING OUTPUT");
         try {
             p = new parser(new ToyScanner("ToyTestFile.toy"));
@@ -47,9 +48,48 @@ public class Project2 {
         }
 
         //ToyTestFile4 is a test of incorrect syntax for for loops. It will never accept.
+        //Syntax error is that the for loop doesn't contain the middle mandatory expression.
         System.out.println("\nFOURTH FILE PARSING OUTPUT");
         try {
             p = new parser(new ToyScanner("ToyTestFile4.toy"));
+            p.parse();
+            System.out.println("\n[accept]");
+        } catch (FileNotFoundException e) {
+            System.out.println("Unable to find file.");
+        } catch (Exception e) {
+            System.out.println("\n[reject]");
+        }
+
+        //ToyTestFile5 is a test of incorrect syntax for variable declarations. It will never accept.
+        //Syntax error is that variables cannot be initialized immediately.
+        System.out.println("\nFIFTH FILE PARSING OUTPUT");
+        try {
+            p = new parser(new ToyScanner("ToyTestFile5.toy"));
+            p.parse();
+            System.out.println("\n[accept]");
+        } catch (FileNotFoundException e) {
+            System.out.println("Unable to find file.");
+        } catch (Exception e) {
+            System.out.println("\n[reject]");
+        }
+
+        //ToyTestFile6 is a test of incorrect syntax for interfaces. It will never accept.
+        //Syntax error is that variable declarations are not allowed in interfaces.
+        System.out.println("\nSIXTH FILE PARSING OUTPUT");
+        try {
+            p = new parser(new ToyScanner("ToyTestFile6.toy"));
+            p.parse();
+            System.out.println("\n[accept]");
+        } catch (FileNotFoundException e) {
+            System.out.println("Unable to find file.");
+        } catch (Exception e) {
+            System.out.println("\n[reject]");
+        }
+
+        //ToyTestFile7 is a test of forgetting a parenthese in a function.
+        System.out.println("\nSEVENTH FILE PARSING OUTPUT");
+        try {
+            p = new parser(new ToyScanner("ToyTestFile7.toy"));
             p.parse();
             System.out.println("\n[accept]");
         } catch (FileNotFoundException e) {
